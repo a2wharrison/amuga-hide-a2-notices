145add8bc2dff5e6a410c81b1a5c741e9440734d
Dave <dave@asheborocreative.com>
2018-06-05 11:24:42 -0400

    Updated version
    Forgot to update the version number. Updated.

--- amuga-hide-a2-notices.php
+++ amuga-hide-a2-notices.php
@@ -3,7 +3,7 @@
 Plugin Name: Amuga - Hide A2 Notices
 Plugin URI: https://github.com/asheborocreative/amuga-hide-a2-notices
 Description: This plugin hides the "Your login page is now here:" and "Editing of plugin and theme files" notices from A2 Optimized
-Version: 0.1
+Version: 0.1.1
 Author: Amuga Themes
 Author URI: https://amugathemes.com
 License: GPL3
